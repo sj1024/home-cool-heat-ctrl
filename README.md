25259fa67ddd31b0e2d7a9d95bcfa93cc4e297e8
SJ Yoon <sjynqw@icloud.com>
2018-08-06 13:23:17 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a Arduino project controlling home air condition can be heater either cooler.
+This project is using: 
+* Wemos Mini
+* High power relay
+* DHT12 sensor
+
+
---
+++ libraries/MyConfig/Wemospin.h
@@ -0,0 +1,11 @@
+// vim: set rnu sw=4 ss=4 ts=4 et smartindent autoindent fdm=indent :
+#define WEMOS_PIN_D0    16
+#define WEMOS_PIN_D1    5
+#define WEMOS_PIN_D2    4
+#define WEMOS_PIN_D3    0
+#define WEMOS_PIN_D4    2 //LED
+#define WEMOS_PIN_LED   2 //LED
+#define WEMOS_PIN_D5    14
+#define WEMOS_PIN_D6    12
+#define WEMOS_PIN_D7    13
+#define WEMOS_PIN_D8    15
